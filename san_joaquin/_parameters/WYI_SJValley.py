ea67b5a7611d230e8ace46e5afbecdd0c712e673
David Rheinheimer <herr.rhein@gmail.com>
2019-10-13 20:39:58 -0500

    fix SJV WYI routine

--- san_joaquin/_parameters/WYI_SJValley.py
+++ san_joaquin/_parameters/WYI_SJValley.py
@@ -7,8 +7,10 @@ class WYI_SJValley(WaterLPParameter):
     """"""
 
     def value(self, timestep, scenario_index):
-        return self.read_csv("Scenarios/Livneh/WYT/SJVI.csv", parse_dates=False, squeeze=True)\
-            [timestep.year if timestep.month < 10 else timestep.year + 1]
+        sjvi = self.read_csv("Scenarios/Livneh/WYT/SJVI.csv", parse_dates=False, squeeze=True)
+        water_year = timestep.year if timestep.month < 10 else timestep.year + 1
+        print(water_year)
+        return sjvi[water_year]
 
     @classmethod
     def load(cls, model, data):
