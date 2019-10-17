16eb9b432003004df1222bbc7de9e09835d975d7
Dan <dantran2016@gmail.com>
2019-10-17 10:36:32 -0700

    Added new IFRs

---
+++ stanislaus/_parameters/node_Below_Beaver_Creek_Diversion_Dam_Requirement.py
@@ -0,0 +1,22 @@
+import datetime
+from parameters import WaterLPParameter
+
+from utilities.converter import convert
+
+
+class node_Below_Beaver_Creek_Diversion_Dam_Requirement(WaterLPParameter):
+    """"""
+
+    def _value(self, timestep, scenario_index):
+        return 0
+
+    def value(self, timestep, scenario_index):
+        return convert(self._value(timestep, scenario_index), "m^3 s^-1", "m^3 day^-1", scale_in=1, scale_out=1000000.0)
+
+    @classmethod
+    def load(cls, model, data):
+        return cls(model, **data)
+
+
+node_Below_Beaver_Creek_Diversion_Dam_Requirement.register()
+print(" [*] node_IFR_bl_Angels_Div_Requirement successfully registered")
---
+++ stanislaus/_parameters/node_IFR_below_Beardsley_Afterbay_Requirement.py
@@ -0,0 +1,21 @@
+import datetime
+from parameters import WaterLPParameter
+from utilities.converter import convert
+
+class node_IFR_below_Beardsley_Afterbay_Requirement(WaterLPParameter):
+    """"""
+
+    def _value(self, timestep, scenario_index):
+        return 0
+
+
+    def value(self, timestep, scenario_index):
+        return convert(self._value(timestep, scenario_index), "m^3 s^-1", "m^3 day^-1", scale_in=1, scale_out=1000000.0)
+
+    @classmethod
+    def load(cls, model, data):
+        return cls(model, **data)
+
+
+node_IFR_below_Beardsley_Afterbay_Requirement.register()
+print(" [*] node_IFR_below_Beardsley_Afterbay_Requirement successfully registered")
