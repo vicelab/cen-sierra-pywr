2537c8622b8eb618713a8e3eb124cde06d42dd91
asood12 <42590427+asood12@users.noreply.github.com>
2019-10-21 13:29:31 -0700

    Changed IFR logic. Modified turbine capacity in JSON file

---
+++ stanislaus/_parameters/node_IFR_bl_Utica_Res_Requirement.py
@@ -0,0 +1,26 @@
+import datetime
+import calendar
+from parameters import WaterLPParameter
+
+from utilities.converter import convert
+
+
+class node_IFR_bl_Utica_Res_Requirement(WaterLPParameter):
+    """"""
+
+    def _value(self, timestep, scenario_index):
+        ifr_val = 0.14158 #cms(5 cfs)
+        if self.mode == 'planning':
+            ifr_val *= self.days_in_planning_month(timestep, self.month_offset)
+        return ifr_val
+
+    def value(self, timestep, scenario_index):
+        return convert(self._value(timestep, scenario_index), "m^3 s^-1", "m^3 day^-1", scale_in=1, scale_out=1000000.0)
+
+    @classmethod
+    def load(cls, model, data):
+        return cls(model, **data)
+
+
+node_IFR_bl_Utica_Res_Requirement.register()
+print(" [*] node_IFR_bl_Utica_Res_Requirement successfully registered")
