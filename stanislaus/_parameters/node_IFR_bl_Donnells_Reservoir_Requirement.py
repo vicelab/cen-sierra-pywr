abc745c167cdaf3e75bc50f8b153e22c82dbec22
Dan <dantran2016@gmail.com>
2019-10-21 14:38:34 -0700

    Fixed requirement logic + added scheduling logic
    -Still has a bug : unsupported operand type(s) for &: 'datetime.date' and 'datetime.date'

--- stanislaus/_parameters/node_IFR_bl_Donnells_Reservoir_Requirement.py
+++ stanislaus/_parameters/node_IFR_bl_Donnells_Reservoir_Requirement.py
@@ -28,15 +28,26 @@ class node_IFR_bl_Donnells_Reservoir_Requirement(WaterLPParameter):
 
         data_supp = self.read_csv(path + 'IFR_Below Donnells Reservoir (Supp)_cfs_daily.csv',
                                   names=['Day_st', 'Day_end', '1', '2', '3', '4', '5'], header=0, index_col=None)
-        if timestep.month == 10 and timestep.day == 1:
-            peak_inflow_Donnells = self.read_csv('Scenarios/Livneh/runoff/peak_runoff_DonnellsRes_MarToMay_cms.csv',
-                                             usecols=[0, 1], index_col=[0], parse_dates=[1], squeeze=True)  # cms
-            self.peak_dt = peak_inflow_Donnells[timestep.year+1]
-        diff_day = (timestep.datetime - self.peak_dt).days
-        ifr_supp = 0
-        if diff_day > 0 and diff_day <=91:
-            ifr_supp = (data_supp[(data_supp['Day_st'] <= diff_day) & (diff_day < data_supp['Day_end'])][str(WYT)])/35.314666
-        return ifr_val + ifr_supp
+        if self.mode == 'scheduling':
+            if timestep.month == 10 and timestep.day == 1:
+                peak_inflow_Donnells = self.read_csv('Scenarios/Livneh/runoff/peak_runoff_DonnellsRes_MarToMay_cms.csv',
+                                                     usecols=[0, 1], index_col=[0], parse_dates=[1],
+                                                     squeeze=True)  # cms
+                self.peak_dt = peak_inflow_Donnells[timestep.year + 1]
+            diff_day = (timestep.datetime - self.peak_dt).days
+            ifr_supp = 0
+            if diff_day > 0 and diff_day <= 91:
+                ifr_supp = (data_supp[(data_supp['Day_st'] <= diff_day) & (diff_day < data_supp['Day_end'])][str(WYT)]).values[-1] / 35.314666
+            return ifr_val + ifr_supp
+        else:
+            if timestep.month == 5:
+                data_supp[str(WYT)].loc[0:4].mean()
+            elif timestep.month == 6:
+                data_supp[str(WYT)].loc[5:8].mean()
+            elif timestep.month == 7:
+                data_supp[str(WYT)].loc[8:12].mean()
+            else:
+                return 0
         
     def value(self, timestep, scenario_index):
         return convert(self._value(timestep, scenario_index), "m^3 s^-1", "m^3 day^-1", scale_in=1, scale_out=1000000.0)
