2253eef0f49bd95aae0dc8653f83d64d0b5db791
David Rheinheimer <herr.rhein@gmail.com>
2019-10-21 21:02:18 -0500

    overhaul of optimization scheme

--- run_basin_model.py
+++ run_basin_model.py
@@ -4,6 +4,10 @@ import json
 from pywr.core import Model
 from importlib import import_module
 from tqdm import tqdm
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+# from pandas import ExcelWriter
 
 
 def setup_model(root_dir, model_path, bucket=None, network_key=None, check_graph=False):
@@ -40,7 +44,7 @@ def setup_model(root_dir, model_path, bucket=None, network_key=None, check_graph
     return
 
 
-def prepare_planning_model(path, outpath, steps=12):
+def prepare_planning_model(path, outpath, steps=12, debug=False):
     with open(path) as f:
         m = json.load(f)
 
@@ -59,7 +63,9 @@ def prepare_planning_model(path, outpath, steps=12):
     gauges = {}
 
     parameters_to_expand = []
+    parameters_to_delete = []
     black_list = ['min_volume', 'max_volume']
+    storage_recorders = {}
 
     for node in m['nodes']:
         old_name = node['name']
@@ -70,6 +76,8 @@ def prepare_planning_model(path, outpath, steps=12):
 
         for key, value in node.items():
             if key in black_list:
+                continue
+            if node_type == 'Storage' and key == 'cost':
                 continue
             if type(value) == str and value in m['parameters']:
                 if value not in parameters_to_expand:
@@ -90,35 +98,91 @@ def prepare_planning_model(path, outpath, steps=12):
 
             month = '/{}'.format(t)
 
-            for key, value in node.items():
-                if type(value) == str and value in m['parameters']:
-                    if key not in black_list:
-                        new_node[key] += month
+            if node_type == 'Storage' and node.get('max_volume'):
+                # 1. rename and empty storage for original storage node
+                for key in ['cost', 'min_volume', 'max_volume', 'initial_volume']:
+                    new_node.pop(key, None)
+                    new_node['max_volume'] = 0.0
+                    new_node['initial_volume'] = 0.0
+                new_nodes.append(new_node)
 
-                elif type(value) == list:
-                    new_values = []
-                    for v in value:
-                        if type(v) == str:
-                            if v not in parameters_to_expand:
-                                parameters_to_expand.append(v)
-                            parts = v.split('/')
-                            if len(parts) == 3:
-                                new_values.append(v + month)
-                            elif len(parts) == 4:
-                                attr = parts[-2]
-                                block = parts[-1]
-                                new_v = '{}/{}/{}/{}/{}'.format(res_class, res_name, attr, t, block)
-                                new_values.append(new_v)
+                # 2. add a catchment to represent initial storage
+                if step == 0:
+                    input_name = old_name + ' [input]'
+                    storage_input = {
+                        'name': input_name,
+                        'type': 'Catchment',
+                        'flow': node.get('initial_volume', 0.0)
+                    }
+                    new_nodes.append(storage_input)
+                    # 2.1 connect inflow to new "reservoir"
+                    new_edges.append([input_name, new_res_name])
+
+                # 3. create new storage node as link
+                link_reservoir_base_name = old_name + ' [link]'
+                link_reservoir_name = link_reservoir_base_name + '/{}'.format(t)
+                storage_link = {
+                    'name': link_reservoir_name,
+                    'type': 'Link',
+                }
+                if 'min_volume' in node:
+                    storage_link['min_flow'] = node['min_volume']
+                if 'max_volume' in node:
+                    storage_link['max_flow'] = node['max_volume']
+                # if 'cost' in node:
+                #     storage_link['cost'] = node['cost']
+                # for now, set cost to zero (by omission)
+                new_nodes.append(storage_link)
+
+                # 3.1 connect this reservoir to new link reservoir
+                new_edges.append([new_res_name, link_reservoir_name])
+
+                # 3.2 connect new link reservoir to next reservoir
+                if step != all_steps[-1]:
+                    new_edges.append([link_reservoir_name, next_res_name])
+
+                # 3.3 note we need to update storage recorders
+                storage_recorders[node['name']] = link_reservoir_base_name
+
+                # 4. create outflow node for reservoir
+                if step == all_steps[-1]:
+                    output_name = old_name + ' [output]'
+                    storage_output = {
+                        'name': output_name,
+                        'type': 'Output'
+                    }
+                    new_nodes.append(storage_output)
+                    # 4.1 connect outflow to new "reservoir"
+                    new_edges.append([link_reservoir_name, output_name])
+
+            else:
+                for key, value in node.items():
+
+                    if type(value) == str and value in m['parameters']:
+                        if key not in black_list:
+                            new_node[key] += month
+
+                    elif type(value) == list:
+                        new_values = []
+                        for v in value:
+                            if type(v) == str:
+                                if v not in parameters_to_expand:
+                                    parameters_to_expand.append(v)
+                                parts = v.split('/')
+                                if len(parts) == 3:
+                                    new_values.append(v + month)
+                                elif len(parts) == 4:
+                                    attr = parts[-2]
+                                    block = parts[-1]
+                                    new_v = '{}/{}/{}/{}/{}'.format(res_class, res_name, attr, t, block)
+                                    new_values.append(new_v)
+                                else:
+                                    new_values.append(v)
                             else:
                                 new_values.append(v)
-                        else:
-                            new_values.append(v)
-                    new_node[key] = new_values
+                        new_node[key] = new_values
 
-            new_nodes.append(new_node)
-
-            if t < steps and node['type'] == 'Storage':
-                new_edges.append([new_res_name, next_res_name])
+                new_nodes.append(new_node)
 
     edges_temp = []
     for n1, n2 in m['edges']:
@@ -137,6 +201,14 @@ def prepare_planning_model(path, outpath, steps=12):
             new_edges.append(['{}/{}'.format(n1, t), '{}/{}'.format(n2, t)])
 
     for param_name in m['parameters']:
+
+        if param_name in parameters_to_delete:
+            continue
+
+        # Delete storage value (at least for now)
+        if '/Storage Value' in param_name:
+            continue
+
         parts = param_name.split('/')
         param = m['parameters'][param_name]
         res_class = None
@@ -177,8 +249,20 @@ def prepare_planning_model(path, outpath, steps=12):
         if 'node' in recorder:
             if recorder['node'] in gauges:
                 continue
-            recorder['node'] += '/1'  # record just the first time step results
-            new_recorders[recorder_name] = recorder
+            if recorder['node'] in storage_recorders:
+                recorder['type'] = 'NumpyArrayNodeRecorder'
+                recorder['node'] = storage_recorders[recorder['node']]
+            elif '/storage' in recorder_name:
+                continue # old storage nodes will be zero storage
+            if not debug:
+                recorder['node'] += '/1'  # record just the first time step results
+                new_recorders[recorder_name] = recorder
+            else:
+                for step in all_steps:
+                    t = step + 1
+                    new_recorder = recorder.copy()
+                    new_recorder['node'] += '/{}'.format(t)
+                    new_recorders['{}/{}'.format(recorder_name, t)] = new_recorder
 
     m['nodes'] = new_nodes
     m['edges'] = new_edges
@@ -190,21 +274,20 @@ def prepare_planning_model(path, outpath, steps=12):
     return
 
 
-def create_planning_model(model_path):
+def create_planning_model(model_path, debug=False):
     root, filename = os.path.split(model_path)
     base, ext = os.path.splitext(filename)
     new_filename = '{}_monthly'.format(base) + ext
     monthly_model_path = os.path.join(root, new_filename)
-    prepare_planning_model(model_path, monthly_model_path)
+    prepare_planning_model(model_path, monthly_model_path, debug=debug)
     # monthly_model = load_model(root_dir, monthly_model_path, bucket=bucket, network_key=network_key, mode='planning')
     monthly_model = Model.load(monthly_model_path, path=monthly_model_path)
     setattr(monthly_model, 'mode', 'planning')
     monthly_model.setup()
-    print('Monthly model setup complete')
     return monthly_model
 
 
-def run_model(basin, network_key):
+def run_model(basin, network_key, debug=False):
     # ========================
     # Set up model environment
     # ========================
@@ -247,10 +330,67 @@ def run_model(basin, network_key):
             print(type(err))
             print(err)
 
+    # Area for testing monthly model
+    if debug == 'm':
+        fn_tpl = 'monthly_{v}_S{level}.csv'
+        print('Creating monthly model...')
+        monthly_model = create_planning_model(model_path, debug=debug=='m')
+        print('...done')
+        print('Setting up monthly model...')
+        monthly_model.setup()
+        print('...done')
+        print('Running monthly model...')
+        monthly_model.timestepper.end -= relativedelta(months=12)
+        start = monthly_model.timestepper.start
+        end = monthly_model.timestepper.end
+        dates = pd.date_range(start=start, end=end, freq='MS') # MS = month start
+
+        nodes_of_type = {}
+        for node in monthly_model.nodes:
+            nodes_of_type[node.type] = node.name
+
+        for date in tqdm(dates, ncols=80, disable=False):
+            print(date)
+            monthly_model.timestepper.start = date
+            # monthly_model.timestepper.end = date
+            monthly_model.step()
+            if date == dates[0]:
+                monthly_dir = './results'
+                # xl_path = os.path.join(monthly_dir, 'monthly_results.xlsx')
+                # with ExcelWriter(xl_path) as xlwriter:
+                df = monthly_model.to_dataframe()
+                df.columns = df.columns.droplevel(1)
+                variables = set([c.split('/')[2] for c in df.columns])
+                for v in variables:
+                    node_names = []
+                    month_numbers = []
+                    col_names = []
+                    for c in df.columns:
+                        res_class, res_name, variable, month_str = c.split('/')
+                        if variable == v:
+                            col_names.append(c)
+                            node_names.append(res_name)
+                            month_numbers.append(int(month_str))
+                    df_filtered = df[col_names]
+                    df_filtered.columns = pd.MultiIndex.from_arrays([node_names, month_numbers])
+                    df0 = df_filtered.stack(level=0)
+                    # df1 = df_filtered.stack(level=1)
+                    path0 = os.path.join(monthly_dir, fn_tpl.format(v=v, level=0))
+                    # path1 = os.path.join(monthly_dir, fn_tpl.format(v=v, level=1))
+                    df0.index.names = ['Start month', 'Resource']
+                    df0.to_csv(path0)
+                    # df1.index.names = ['Start month', 'Planning month']
+                    # df1.to_csv(path1)
+
+                    # df0.to_excel(xlwriter, sheet_name=v)
+
+        print('...done')
+        return
+
     # ==================
     # Create daily model
     # ==================
-    include_monthly = False
+    include_monthly = True
     daily_model = Model.load(model_path, path=model_path)
     print('Daily model loaded')
     daily_model.setup()
@@ -340,11 +480,13 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--basin", help="Basin to run")
 parser.add_argument("-nk", "--network_key", help="Network key")
+parser.add_argument("-d", "--debug", help="Debug ('m' or 'd')")
 args = parser.parse_args()
 
 basin = args.basin
 network_key = args.network_key or os.environ.get('NETWORK_KEY')
+debug = args.debug
 
-run_model(basin, network_key)
+run_model(basin, network_key, debug)
 
 print('done!')
