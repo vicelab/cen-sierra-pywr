aa0d2a25d38dee85fcd77ef5ceff1a4647c0f68e
David Rheinheimer <herr.rhein@gmail.com>
2019-10-24 14:31:15 -0500

    fix tulloch lake release policy

--- stanislaus/policies/node_Release_From_Tulloch_Lake.py
+++ stanislaus/policies/node_Release_From_Tulloch_Lake.py
@@ -8,7 +8,7 @@ class node_Release_From_Tulloch_Lake(WaterLPParameter):
 
         wyt_number = self.model.parameters['WYT_SJValley'].value(timestep, scenario_index)
         wyt = self.year_names[wyt_number-1]
-        net_demand = self.read_csv("NetDemand_belowTulloch.csv", index_col=[0], parse_dates=True)
+        net_demand = self.read_csv("Management/BAU/NetDemand_belowTulloch.csv", index_col=[0], parse_dates=True)
 
         day = timestep.day if timestep.month != 2 else min(28, timestep.day)
         datestring = '1990-{:02}-{:02}'.format(timestep.month, day)
@@ -17,7 +17,7 @@ class node_Release_From_Tulloch_Lake(WaterLPParameter):
     def _value(self, timestep, scenario_index):
         max_outflow = 226.535
         # Reservoir Node (Tulloch)
-        storage_name = "Tulloch Lake [node]" + self.month_suffix
+        storage_name = "Tulloch Lake" + self.month_suffix
         storage_demand_name = "node/Tulloch Lake/Storage Demand"
         # Inflow into Tulloch Lake
         inflow_name = "STN_below_Melons.2.2" + self.month_suffix
