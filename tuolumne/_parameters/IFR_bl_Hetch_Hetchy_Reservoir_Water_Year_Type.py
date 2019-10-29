b715c819aaee9d75f8d5c31c611a8a8fefcfa165
David Rheinheimer <herr.rhein@gmail.com>
2019-10-28 22:17:23 -0600

    update IFR bl HH WYT calculation

--- tuolumne/_parameters/IFR_bl_Hetch_Hetchy_Reservoir_Water_Year_Type.py
+++ tuolumne/_parameters/IFR_bl_Hetch_Hetchy_Reservoir_Water_Year_Type.py
@@ -1,5 +1,5 @@
 from parameters import WaterLPParameter
-
+from datetime import datetime
 from utilities.converter import convert
 
 class IFR_bl_Hetch_Hetchy_Reservoir_Water_Year_Type(WaterLPParameter):
@@ -7,49 +7,48 @@ class IFR_bl_Hetch_Hetchy_Reservoir_Water_Year_Type(WaterLPParameter):
 
     def _value(self, timestep, scenario_index):
         kwargs = dict(timestep=timestep, scenario_index=scenario_index)
-        # get cumulative precip
-        if timestep.month >= 10:
-            start_year = timestep.year
-        else:
-            start_year = timestep.year - 1
-        start = '{}-10-10'.format(start_year)
-        
-        # get criteria
-        schedule = self.model.parameters["IFR bl Hetch Hetchy Reservoir/IFR Schedule"].array()
-        row = timestep.month + 1
-        criteria = [schedule[row][1], schedule[row][3]]
-        
-        # iniitial IFR
-        if timestep.datetime == self.dates[0]:
-            WYT = 2 # default
+
+        # initial IFR
+        WYT = 2 # default
         
         # These are monthly values, so only calculate values in the first time step of each month
-        elif timestep.day > 10 or timestep.month in [9,10,11,12]:
-            WYT = self.get("IFR bl Hetch Hetchy Reservoir/Water Year Type", offset=-1, **kwargs)
+        if timestep.month >= 9:
+            WYT = self.WYT
         
         # Jan-June:
-        elif timestep.month in [1,2,3,4,5,6]:
-            cumulative_precip = self.get("node/HETCH_HETCHY_RES/Precipitation", start=start, agg='sum')
-            if cumulative_precip >= criteria[0]:
-                WYT = 3
-            elif cumulative_precip >= criteria[1]:
-                WYT = 2
+        else:
+            schedule = self.model.parameters["IFR bl Hetch Hetchy Reservoir/IFR Schedule"].array()
+            if timestep.month < 9 or timestep.month == 9 and timestep.day <= 15:
+                row = timestep.month + 1
             else:
-                WYT = 1
-                
-        # July-Aug:
-        elif timestep.month in [7,8]:
-            timesteps_to_date = timestep % 36 + 1
-            average_cfs = self.get("node/HETCH_HETCHY_WATERSHED/Runoff", start=start, agg='mean')
-            cumulative_af = average_cfs * 60*60*24*10*timesteps_to_date / 43559.9 / 1000
-            
-            if cumulative_af >= criteria[0]:
-                WYT = 3
-            elif cumulative_af >= criteria[1]:
-                WYT = 2
-            else:
-                WYT = 1
-            
+                row = timestep.month + 2
+            criteria = (schedule[row, 2], schedule[row, 4])
+
+            if timestep.month <= 6:
+                # precip = self.get("Hetch Hetchy Reservoir/Precipitation").dataframe
+                # total_precip = precip[datetime(timestep.year - 1, 10, 1):timestep.datetime].sum()
+                total_precip = 1000
+                if total_precip >= criteria[0]:
+                    WYT = 3
+                elif total_precip >= criteria[1]:
+                    WYT = 2
+                else:
+                    WYT = 1
+
+            # July-Aug:
+            elif timestep.month in [7,8]:
+                runoff = self.get("TUO_13 Headflow/Runoff").dataframe
+                cumulative_runoff = runoff[datetime(timestep.year - 1, 10, 1):timestep.datetime].sum()
+                cumulative_runoff *= 810.7 # convert mcm to af
+                if cumulative_runoff >= criteria[0]:
+                    WYT = 3
+                elif cumulative_runoff >= criteria[1]:
+                    WYT = 2
+                else:
+                    WYT = 1
+
+        self.WYT = WYT
+
         return WYT
         
     def value(self, timestep, scenario_index):
