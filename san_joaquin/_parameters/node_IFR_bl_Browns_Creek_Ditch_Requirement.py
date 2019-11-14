852f118e862be546eff009c6211040b58d3b6f21
Dan <dantran2016@gmail.com>
2019-11-14 11:59:55 -0800

    Update node_IFR_bl_Browns_Creek_Ditch_Requirement.py

--- san_joaquin/_parameters/node_IFR_bl_Browns_Creek_Ditch_Requirement.py
+++ san_joaquin/_parameters/node_IFR_bl_Browns_Creek_Ditch_Requirement.py
@@ -6,32 +6,40 @@ class node_IFR_bl_Browns_Creek_Ditch_Requirement(WaterLPParameter):
     """"""
 
     def _value(self, timestep, scenario_index):
+        # Get Year Type
         year_type = self.model.parameters["WYT_SJValley"].values(timestep, scenario_index)
 
         if year_type in [1,2]:  # Critical or Dry WYT
             return 3
 
+        # Get month
         month = self.datetime.month
         prescribed = 0
 
+        # Month = Jan
         if month == 1:
             prescribed = 4.5
+        # Month = Feb
         elif month == 2:
             prescribed = 8
+        # Month = Mar - Apr
         elif month == 3 or month == 4:
             prescribed = 10
+        # Month = May
         elif month == 5:
             prescribed = 8
+        # Other
         else:
             prescribed = 4.5
 
+        # Account for planning model
         if self.model.mode == "planning":
             prescribed *= self.days_in_month()
 
         return prescribed
         
     def value(self, timestep, scenario_index):
-        return convert(self._value(timestep, scenario_index), "m^3 s^-1", "m^3 day^-1", scale_in=1, scale_out=1000000.0)
+        return convert(self._value(timestep, scenario_index), "cfs", "m^3 day^-1", scale_in=1, scale_out=1000000.0)
 
     @classmethod
     def load(cls, model, data):
