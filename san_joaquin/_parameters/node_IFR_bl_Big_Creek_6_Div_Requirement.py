58fcaadcf957ac338c2d066ea06e7f06bf318e2c
Dan <dantran2016@gmail.com>
2019-11-18 15:45:44 -0800

    Added more IFR logic including node_IFR_bl_Redinger_Lake_Requirement

--- san_joaquin/_parameters/node_IFR_bl_Big_Creek_6_Div_Requirement.py
+++ san_joaquin/_parameters/node_IFR_bl_Big_Creek_6_Div_Requirement.py
@@ -6,10 +6,10 @@ class node_IFR_bl_Big_Creek_6_Div_Requirement(WaterLPParameter):
     """"""
 
     def _value(self, timestep, scenario_index):
-        return_value = 3
+        return_val = 3
         if self.model.mode == "planning":
-            return_value *= self.days_in_month()
-        return return_value
+            return_val *= self.days_in_month()
+        return return_val
         
     def value(self, timestep, scenario_index):
         return convert(self._value(timestep, scenario_index), "cfs", "m^3 day^-1", scale_in=1, scale_out=1000000.0)
