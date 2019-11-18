ab2c9ecf3b0a9cfe23bd3fc222b33698b1012c46
Dan <dantran2016@gmail.com>
2019-11-18 14:38:19 -0800

    Added node_IFR_bl_Bear_Div_Requirement

--- san_joaquin/_parameters/node_IFR_bl_Bear_Div_Requirement.py
+++ san_joaquin/_parameters/node_IFR_bl_Bear_Div_Requirement.py
@@ -6,14 +6,25 @@ class node_IFR_bl_Bear_Div_Requirement(WaterLPParameter):
     """"""
 
     def _value(self, timestep, scenario_index):
-        # TODO Update Logic
-        management = 'BAU'
-        path = "Management/{mgt}/IFRs/IFRblBearDiv.csv".format(mgt=management)
-        data = self.read_csv(path, usecols=[0,1,2,3,4,5], index_col=0, header=None, names=['week','1','2','3','4','5'], parse_dates=False)
-        WYT = self.model.parameters['WYT_SJValley'].value(timestep, scenario_index)
-        week = min(timestep.datetime.weekofyear, 52)
-        ifr = data[str(WYT)][week]
-        return convert(ifr, 'ft^3 s^-1', 'hm^3 day^-1')
+        year_type = self.model.parameters["WYT_SJValley"].values(timestep, scenario_index)
+        month = self.datetime.month
+        return_val = 0
+
+        if year_type in [1, 2]:  # Critical or Dry WYT
+            if month in [5,6,7,8,9]:
+                return_val = 2
+            else:
+                return_val = 1
+        else:
+            if month in [5,6,7,8,9]:
+                return_val = 3
+            else:
+                return_val = 2
+
+        if self.model.mode == "planning":
+            return_val *= self.days_in_month()
+
+        return return_val
         
     def value(self, timestep, scenario_index):
         return convert(self._value(timestep, scenario_index), "m^3 s^-1", "m^3 day^-1", scale_in=1, scale_out=1000000.0)
