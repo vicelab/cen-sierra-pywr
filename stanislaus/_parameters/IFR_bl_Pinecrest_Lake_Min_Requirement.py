8d0ef4f2ed13e907bf8b1d45fd8262ce56bad07f
David Rheinheimer <herr.rhein@gmail.com>
2019-11-30 15:18:52 -0800

    add ramping rate objectives to below Pinecrest Dam & Phila. Div. Dam

--- stanislaus/_parameters/IFR_bl_Pinecrest_Lake_Min_Requirement.py
+++ stanislaus/_parameters/IFR_bl_Pinecrest_Lake_Min_Requirement.py
@@ -4,7 +4,7 @@ from parameters import WaterLPParameter
 from utilities.converter import convert
 
 
-class IFR_bl_Philadelphia_Div_Requirement(WaterLPParameter):
+class IFR_bl_Pinecrest_Lake_Min_Requirement(WaterLPParameter):
     """"""
 
     def _value(self, timestep, scenario_index):
@@ -13,9 +13,9 @@ class IFR_bl_Philadelphia_Div_Requirement(WaterLPParameter):
         management = "BAU"
         path = "Management/{mgt}/IFRs/".format(mgt=management)
         if self.mode == 'scheduling':
-            fName = 'IFR_Below Philadelphia Div. (MIF)_cfs_daily.csv'
+            fName = 'IFR_Below Pinecrest Lake_cfs_daily.csv'
         else:
-            fName = 'IFR_Below Philadelphia Div. (MIF)_cfs_monthly.csv'
+            fName = 'IFR_Below Pinecrest Lake_cfs_monthly.csv'
         if self.datetime.month >= 10:
             dt = datetime.date(1999, self.datetime.month, self.datetime.day)
         else:
@@ -24,9 +24,12 @@ class IFR_bl_Philadelphia_Div_Requirement(WaterLPParameter):
         data = self.read_csv(path + fName, usecols=[0, 1, 2, 3, 4, 5, 6], index_col=None, header=0,
                              names=['start_date', 'end_date', '1', '2', '3', '4', '5'], parse_dates=[0, 1])
         # Critically Dry: 1,Dry: 2,Normal-Dry: 3,Normal-Wet: 4,Wet: 5
-        ifr_val = (data[(data['start_date'] <= dt) & (data['end_date'] >= dt)][str(WYT)]).values[-1] / 35.314666
+        ifr_val = (data[(data['start_date'] <= dt) & (data['end_date'] >= dt)][str(WYT)]) / 35.314666
 
-        if self.model.mode == 'planning':
+        if self.model.mode == 'scheduling':
+            ifr_val = self.get_down_ramp_ifr(timestep, ifr_val, initial_value=10/35.31, rate=0.25)
+
+        elif self.model.mode == 'planning':
             ifr_val *= self.days_in_month()
 
         return ifr_val
@@ -45,5 +48,5 @@ class IFR_bl_Philadelphia_Div_Requirement(WaterLPParameter):
         return cls(model, **data)
 
 
-IFR_bl_Philadelphia_Div_Requirement.register()
-print(" [*] IFR_bl_Philadelphia_Div_Requirement successfully registered")
+IFR_bl_Pinecrest_Lake_Min_Requirement.register()
+print(" [*] IFR_bl_Pinecrest_Lake_Min_Requirement successfully registered")
