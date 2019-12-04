29216231c46d53dffa481a1acd32d681dd295274
David Rheinheimer <herr.rhein@gmail.com>
2019-12-04 09:00:29 -0800

    stanislaus improvements

--- stanislaus/_parameters/IFR_bl_Pinecrest_Lake_Min_Requirement.py
+++ stanislaus/_parameters/IFR_bl_Pinecrest_Lake_Min_Requirement.py
@@ -10,19 +10,22 @@ class IFR_bl_Pinecrest_Lake_Min_Requirement(WaterLPParameter):
     def _value(self, timestep, scenario_index):
 
         WYT = self.get("San Joaquin Valley WYT" + self.month_suffix)
-        management = "BAU"
-        path = "Management/{mgt}/IFRs/".format(mgt=management)
-        if self.mode == 'scheduling':
-            fName = 'IFR_Below Pinecrest Lake_cfs_daily.csv'
-        else:
-            fName = 'IFR_Below Pinecrest Lake_cfs_monthly.csv'
+        # management = "BAU"
+        # path = "Management/{mgt}/IFRs/".format(mgt=management)
+        # if self.mode == 'scheduling':
+        #     fName = 'IFR_Below Pinecrest Lake_cfs_daily.csv'
+        # else:
+        #     fName = 'IFR_Below Pinecrest Lake_cfs_monthly.csv'
         if self.datetime.month >= 10:
             dt = datetime.date(1999, self.datetime.month, self.datetime.day)
         else:
             dt = datetime.date(2000, self.datetime.month, self.datetime.day)
 
-        data = self.read_csv(path + fName, usecols=[0, 1, 2, 3, 4, 5, 6], index_col=None, header=0,
-                             names=['start_date', 'end_date', '1', '2', '3', '4', '5'], parse_dates=[0, 1])
+        # data = self.read_csv(path + fName, usecols=[0, 1, 2, 3, 4, 5, 6], index_col=None, header=0,
+        #                      names=['start_date', 'end_date', '1', '2', '3', '4', '5'], parse_dates=[0, 1])
+
+        data = self.model.tables["IFR Below Pinecrest Lake schedule"]
+
         # Critically Dry: 1,Dry: 2,Normal-Dry: 3,Normal-Wet: 4,Wet: 5
         ifr_val = float(data[(data['start_date'] <= dt) & (data['end_date'] >= dt)][str(WYT)]) / 35.314666
 
