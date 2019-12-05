b1a593078c4957760122541fbe4371208ecb7e7e
David Rheinheimer <herr.rhein@gmail.com>
2019-12-04 17:17:15 -0800

    try to add multiprocessing

--- run_basin_model.py
+++ run_basin_model.py
@@ -11,6 +11,8 @@ from dateutil.relativedelta import relativedelta
 from common.tests import test_planning_model, get_planning_dataframe
 import numpy as np
 import pandas as pd
+import multiprocessing as mp
+from functools import partial
 
 SECONDS_IN_DAY = 3600 * 24
 
@@ -483,9 +485,24 @@ def prepare_planning_model(m, outpath, steps=12, blocks=8, debug=False):
     return
 
 
-def run_model(basin, scenario, start, end, network_key, run_name="default", include_planning=False, simplify=True,
+def run_model(basin, scenario, network_key=None, start=None, end=None, run_name="default", include_planning=False,
+              simplify=True,
               debug=False, planning_months=12):
     months = planning_months
+
+    if start is None or end is None:
+        climate, price_year = scenario
+        if climate == 'Livneh':
+            start_year = 2000
+            if debug:
+                end_year = 2003
+            else:
+                end_year = 2012
+        else:
+            start_year = 2030
+            end_year = 2045
+        start = '{}-10-01'.format(start_year)
+        end = '{}-09-30'.format(end_year)
 
     # ========================
     # Set up model environment
@@ -763,12 +780,14 @@ parser.add_argument("-nk", "--network_key", help="Network key")
 parser.add_argument("-d", "--debug", help="Debug ('m' or 'd' or 'dm')")
 parser.add_argument("-p", "--include_planning", help="Include planning model", action='store_true')
 parser.add_argument("-n", "--run_name", help="Run name")
+parser.add_argument("-mp", "--multiprocessing", help="Use multiprocessing", action='store_true')
 args = parser.parse_args()
 
 basin = args.basin
 network_key = args.network_key or os.environ.get('NETWORK_KEY')
 debug = args.debug
 include_planning = args.include_planning
+multiprocessing = args.multiprocessing
 
 gcms = ['HadGEM2-ES', 'CNRM-CM5', 'CanESM2', 'MIROC5']
 # gcms = ['HadGEM2-ES', 'MIROC5']
@@ -777,8 +796,10 @@ gcm_rcps = ['{}_rcp{}'.format(g, r) for g, r in product(gcms, rcps)]
 
 if debug:
     planning_months = 2
-    climate_scenarios = ['Livneh']
-    price_years = [2009]
+    # climate_scenarios = ['Livneh']
+    # price_years = [2009]
+    climate_scenarios = ['CanESM2_rcp85']
+    price_years = [2060]
 else:
     planning_months = 12
     climate_scenarios = ['Livneh'] + gcm_rcps
@@ -786,31 +807,30 @@ else:
 
 scenarios = product(climate_scenarios, price_years)
 
-for scenario in scenarios:
-    print('Running: ', scenario)
-    climate, price_year = scenario
-    if climate == 'Livneh':
-        start_year = 2000
-        if debug:
-            end_year = 2003
-        else:
-            end_year = 2012
-    else:
-        start_year = 2030
-        end_year = 2045
-    start = '{}-10-01'.format(start_year)
-    end = '{}-09-30'.format(end_year)
-    try:
-        run_model(
-            basin, scenario, start, end, network_key,
-            run_name=args.run_name,
-            include_planning=include_planning,
-            debug=debug,
-            planning_months=planning_months
-        )
-    except Exception as err:
-        print("Failed: ", scenario)
-        print(err)
-        continue
+kwargs = dict(
+    run_name=args.run_name,
+    include_planning=include_planning,
+    debug=debug,
+    planning_months=planning_months
+)
+
+if not multiprocessing:  # serial processing for debugging
+    for scenario in scenarios:
+        print('Running: ', scenario)
+        try:
+            run_model(basin, scenario, **kwargs)
+        except Exception as err:
+            print("Failed: ", scenario)
+            print(err)
+            continue
+
+else:
+    pool = mp.Pool(processes=4)
+    run_model_partial = partial(run_model, **kwargs)
+    for scenario in scenarios:
+        pool.apply_async(run_model_partial, args=(basin, scenario))
+
+    pool.close()
+    pool.join()
 
 print('done!')
