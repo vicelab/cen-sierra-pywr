4454f2ef8920079d1753aa88f944e28a652df20d
David Rheinheimer <herr.rhein@gmail.com>
2019-12-07 14:11:30 -0800

    misc updates

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ gcm_rcps = ['{}_rcp{}'.format(g, r) for g, r in product(gcms, rcps)]
 
 if debug:
     planning_months = 2
-    # climate_scenarios = ['Livneh']
+    climate_scenarios = ['Livneh']
     price_years = [2009]
-    climate_scenarios = ['CanESM2_rcp85']
+    # climate_scenarios = ['CanESM2_rcp85']
     # price_years = [2060]
 else:
     planning_months = 12
