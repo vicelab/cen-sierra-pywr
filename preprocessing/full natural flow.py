5fb1cc17d0de7154a47911b7bd369a54dd956dcd
David Rheinheimer <herr.rhein@gmail.com>
2019-12-08 10:32:18 -0800

    add full natural flow preprocessing

---
+++ preprocessing/full natural flow.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+
+root_dir = '../data'
+basins = ['merced', 'stanislaus']
+
+for basin in os.listdir(root_dir):
+    if 'River' not in basin:
+        continue
+    basin_dir = os.path.join(root_dir, basin)
+    basin_runoff_dir = os.path.join(basin_dir, 'Scenarios', 'runoff')
+    if not os.path.exists(basin_runoff_dir):
+        continue
+    print(basin)
+    for scenario in os.listdir(basin_runoff_dir):
+        scenario_dir = os.path.join(basin_runoff_dir, scenario)
+        subwats = []
+        if not os.path.exists(scenario_dir):
+            continue
+        print(scenario)
+        for subwat in os.listdir(scenario_dir):
+            path = os.path.join(scenario_dir, subwat)
+            df = pd.read_csv(path, parse_dates=True, index_col=0, header=0)
+            subwats.append(df)
+        df = pd.concat(subwats, axis=1).sum(axis=1).to_frame()
+        df.index.name = 'date'
+        df.columns = ['flow']
+        outdir = os.path.join(basin_dir, 'Scenarios', 'preprocessed', scenario)
+        df.to_csv(os.path.join(outdir, 'full_natural_flow_daily_mcm.csv'))
+        df.resample('MS').sum().to_csv(os.path.join(outdir, 'full_natural_flow_monthly_mcm.csv'))
