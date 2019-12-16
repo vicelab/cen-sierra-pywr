0842ce9371bb8159bb1fb60c343f278a6d8e40cb
David Rheinheimer <herr.rhein@gmail.com>
2019-12-16 12:19:26 -0800

    reinstall pywr

--- main.py
+++ main.py
@@ -28,17 +28,18 @@ gcm_rcps = ['{}_rcp{}'.format(g, r) for g, r in product(gcms, rcps)]
 start = None
 end = None
 if debug:
-    planning_months = 2
+    planning_months = 12
     climate_scenarios = ['Livneh']
     price_years = [2009]
     # climate_scenarios = ['CanESM2_rcp85']
     # price_years = [2060]
-    start = '2000-10-01'
-    end = '2012-09-30'
+    # start = '1990-10-01'
+    # end = '2012-09-30'
 else:
     planning_months = 12
-    climate_scenarios = ['Livneh']  # + gcm_rcps
-    price_years = [2009, 2030, 2045, 2060]
+    # climate_scenarios = ['Livneh']  # + gcm_rcps
+    climate_scenarios = ['HadGEM2-ES_rcp85']  # + gcm_rcps
+    price_years = [2030]
 
 scenarios = product(climate_scenarios, price_years)
 
