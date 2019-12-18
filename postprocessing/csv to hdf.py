dd166099411aa86516592c80cf234788e5dbb172
David Rheinheimer <herr.rhein@gmail.com>
2019-12-17 17:23:06 -0800

    update postprocessing csv to hdf

--- postprocessing/csv to hdf.py
+++ postprocessing/csv to hdf.py
@@ -4,21 +4,25 @@ import pandas as pd
 
 run = 'full run 2019-12-12'
 resultsdir = r'C:\Users\david\Box\CERC-WET\Task7_San_Joaquin_Model\Pywr models\results'
+outdir = r'C:\data'
 rundir = join(resultsdir, run)
 
-hdfpath = join(rundir, 'results.h5')
+hdfpath = join(outdir, '{basin}.h5')
 
-basins = ['merced']
+headers = {
+    'merced': [0, 1, 2],
+    'stanislaus': [0, 1]
+}
+
+basins = ['stanislaus']
 for basin in basins:
+    print(basin)
     basindir = join(rundir, basin)
-    header = [0,1,2]
+    header = headers[basin]
     for result in os.listdir(join(basindir, 'Livneh_P2009')):
-        if '.h5' in result:
-            continue
+        print(result)
         dfs = []
         for scenario in os.listdir(basindir):
-            if '.h5' in scenario:
-                continue
             scendir = join(basindir, scenario)
             path = join(scendir, result)
             df = pd.read_csv(path, parse_dates=True, index_col=0, header=header, skiprows=0)
@@ -26,9 +30,8 @@ for basin in basins:
             new_cols = [tuple([scenario] + list(col)) for col in df.columns]
             df.columns = pd.MultiIndex.from_tuples(new_cols)
             dfs.append(df)
-        df = pd.concat(dfs, axis=1)
+        df = pd.concat(dfs, axis=1, sort=True)
         key = result.split('.')[0]
-        outpath = join(basindir, 'results.h5')
-        df.to_hdf(outpath, key=key.replace(' ', '_'))
+        df.to_hdf(hdfpath.format(basin=basin), key=key.replace(' ', '_'))
 
 print('done!')
