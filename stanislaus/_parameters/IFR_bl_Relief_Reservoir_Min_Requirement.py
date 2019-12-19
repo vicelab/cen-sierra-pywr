0dd387f4a7720ca59bc4af943704bceae023326b
David Rheinheimer <herr.rhein@gmail.com>
2019-12-18 23:45:22 -0800

    upgrade ifr below relief reservoir

--- stanislaus/_parameters/IFR_bl_Relief_Reservoir_Min_Requirement.py
+++ stanislaus/_parameters/IFR_bl_Relief_Reservoir_Min_Requirement.py
@@ -9,37 +9,25 @@ class IFR_bl_Relief_Reservoir_Min_Requirement(WaterLPParameter):
 
     def _value(self, timestep, scenario_index):
 
-        WYT_table = self.model.tables["WYT P2005 & P2130"]
-        if 4 <= self.datetime.month <= 12:
-            operational_water_year = self.datetime.year
-        else:
-            operational_water_year = self.datetime.year - 1
+        WYT = self.model.tables["WYT P2005 & P2130"][self.operational_water_year]
+        schedule = self.model.tables["IFR Below Relief Reservoir schedule"]
 
-        WYT = WYT_table[operational_water_year]
-        management = "BAU"
-        path = "Management/{mgt}/IFRs/".format(mgt=management)
+        month = self.datetime.month
         if self.model.mode == 'scheduling':
-            fName = 'IFR_Below Relief Reservoir (MIF)_cfs_daily.csv'
+            day = self.datetime.day
+            start_day = 1
+            start_month = month
+            if (2, 10) <= (month, day) <= (5, 31):
+                start_day = 10
+            if month in [2, 3, 4, 5] and day <= 9:
+                start_month -= 1
+            ifr_cms = schedule.at[(start_month, start_day), WYT] / 35.31
+            ifr_cms = self.get_down_ramp_ifr(timestep, ifr_cms, rate=0.25)
+
         else:
-            fName = 'IFR_Below Relief Reservoir (MIF)_cfs_monthly.csv'
-        if self.datetime.month >= 10:
-            dt = datetime.date(2000, self.datetime.month, self.datetime.day)
-        else:
-            dt = datetime.date(2000, self.datetime.month, self.datetime.day)
+            ifr_cms = schedule.at[(month, 1), WYT] / 35.31 * self.days_in_month()
 
-        data = self.read_csv(path + fName, usecols=[0, 1, 2, 3, 4, 5, 6], index_col=None, header=0,
-                             names=['start_date', 'end_date', '1', '2', '3', '4', '5'], parse_dates=[0, 1])
-        # Critically Dry: 1,Dry: 2,Normal-Dry: 3,Normal-Wet: 4,Wet: 5
-        ifr_val = data[(data['start_date'] <= dt) & (data['end_date'] >= dt)][str(WYT)].values[-1] / 35.314666 # cfs to cms
-
-        # apply down ramp rate
-        if self.model.mode == 'scheduling':
-            ifr_val = self.get_down_ramp_ifr(timestep, ifr_val, initial_value=30/35.31, rate=0.25)
-
-        elif self.model.mode == 'planning':
-            ifr_val *= self.days_in_month()
-
-        return ifr_val
+        return ifr_cms
 
     def value(self, timestep, scenario_index):
         try:
