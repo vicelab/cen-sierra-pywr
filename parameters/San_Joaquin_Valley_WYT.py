9ffd44a5baeab806cefdc851ce5c9cb05133ddbd
David Rheinheimer <herr.rhein@gmail.com>
2020-01-16 15:59:35 -0800

    move SJV WYT/WYI to global folder

--- parameters/San_Joaquin_Valley_WYT.py
+++ parameters/San_Joaquin_Valley_WYT.py
@@ -2,33 +2,29 @@ from parameters import WaterLPParameter
 
 from utilities.converter import convert
 
-class WYT_SJValley(WaterLPParameter):
+
+class San_Joaquin_Valley_WYT(WaterLPParameter):
     """"""
 
     def _value(self, timestep, scenario_index):
         kwargs = dict(timestep=timestep, scenario_index=scenario_index)
-        SJVI = self.get("WYI_SJValley", **kwargs)
+        SJVI = self.get("San Joaquin Valley WYI" + self.month_suffix, **kwargs)
         thresholds = [0, 2.1, 2.8, 3.1, 3.8]
         WYT = len([x for x in thresholds if SJVI > x])
         return WYT
-        
+
     def value(self, timestep, scenario_index):
         try:
             return self._value(timestep, scenario_index)
         except Exception as err:
-            print('ERROR for parameter {}'.format(self.name))
+            print('\nERROR for parameter {}'.format(self.name))
             print('File where error occurred: {}'.format(__file__))
             print(err)
-            raise
 
     @classmethod
     def load(cls, model, data):
-        try:
-            return cls(model, **data)
-        except Exception as err:
-            print('File where error occurred: {}'.format(__file__))
-            print(err)
-            raise
-        
-WYT_SJValley.register()
-print(" [*] WYT_SJValley successfully registered")
+        return cls(model, **data)
+
+
+San_Joaquin_Valley_WYT.register()
+print(" [*] San_Joaquin_Valley_WYT successfully registered")
