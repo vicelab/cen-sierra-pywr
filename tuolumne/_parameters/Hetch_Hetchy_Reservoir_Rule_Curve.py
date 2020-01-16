18432141cddc90cc49c4001ebe8530b5e4248bde
David Rheinheimer <herr.rhein@gmail.com>
2020-01-15 16:08:58 -0800

    initial tuolumne commit

--- tuolumne/_parameters/Hetch_Hetchy_Reservoir_Rule_Curve.py
+++ tuolumne/_parameters/Hetch_Hetchy_Reservoir_Rule_Curve.py
@@ -2,17 +2,18 @@ from parameters import WaterLPParameter
 
 from utilities.converter import convert
 
-class TUOLUMNE_R_NR_HETCH_HETCHY_CA_11276500_Observed_Flow(WaterLPParameter):
+class Preferred_Storage(WaterLPParameter):
     """"""
 
     def _value(self, timestep, scenario_index):
-        
-        df = self.read_csv("Observed/Streamflow/GaugeStreamFlow_cms_TUO_R3.csv", index_col=0, header=0)
-        return df["11276500"][timestep.datetime]
+        preferred_storage_af = self.model.tables["Preferred Storage"].at[(timestep.month, timestep.day), self.res_name]
+        max_storage_af = self.model.nodes[self.res_name].max_volume * 1e6 / 1233.5
+        preferred_storage_percent = preferred_storage_af / max_storage_af
+        return preferred_storage_percent
         
     def value(self, timestep, scenario_index):
         try:
-            return convert(self._value(timestep, scenario_index), "m^3 s^-1", "m^3 day^-1", scale_in=1, scale_out=1000000.0)
+            return self._value(timestep, scenario_index)
         except Exception as err:
             print('ERROR for parameter {}'.format(self.name))
             print('File where error occurred: {}'.format(__file__))
@@ -28,5 +29,5 @@ class TUOLUMNE_R_NR_HETCH_HETCHY_CA_11276500_Observed_Flow(WaterLPParameter):
             print(err)
             raise
         
-TUOLUMNE_R_NR_HETCH_HETCHY_CA_11276500_Observed_Flow.register()
-print(" [*] TUOLUMNE_R_NR_HETCH_HETCHY_CA_11276500_Observed_Flow successfully registered")
+Preferred_Storage.register()
+print(" [*] Preferred_Storage successfully registered")
