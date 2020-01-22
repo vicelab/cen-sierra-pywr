4f678534b493b9f90d53764aac6f64f368762aeb
David Rheinheimer <herr.rhein@gmail.com>
2020-01-21 20:44:07 -0800

    add demand logic to USJ

--- tuolumne/_parameters/IFR_bl_Hetch_Hetchy_Reservoir_Water_Year_Type.py
+++ tuolumne/_parameters/IFR_bl_Hetch_Hetchy_Reservoir_Water_Year_Type.py
@@ -2,46 +2,47 @@ from parameters import WaterLPParameter
 
 from utilities.converter import convert
 
+
 class IFR_bl_Hetch_Hetchy_Reservoir_Water_Year_Type(WaterLPParameter):
     """"""
 
     def _value(self, timestep, scenario_index):
-        
+
         # initial IFR
         if timestep.index == 0:
             self.WYT = 2  # default
 
         date = self.datetime
-        
+
         # These are monthly values, so only calculate values in the first time step of each month
         if date.month >= 9:
             WYT = self.WYT
-        
+
         # Jan-June:
         else:
             schedule = self.model.tables["IFR bl Hetch Hetchy Reservoir/IFR Schedule"]
 
             lookup_row = timestep.month - 1
             if (date.month, date.day) > (9, 15):
-                 lookup_row += 1
+                lookup_row += 1
 
             criteria = (schedule.iat[lookup_row, 1], schedule.iat[lookup_row, 3])
-        
+
+            oct_1 = '{:04}-10-01'.format(date.year - 1)
+
             if date.month <= 6:
-                # precip = self.get("Hetch Hetchy Reservoir/Precipitation").dataframe
-                # total_precip = precip[datetime(timestep.year - 1, 10, 1):timestep.datetime].sum()
-                total_precip = 1000
+                precip = self.model.parameters["Hetch Hetchy Reservoir/Precipitation"].dataframe
+                total_precip = precip[oct_1:date].sum() / 25.4  # sum & convert mm to inches
                 if total_precip >= criteria[0]:
                     WYT = 3
                 elif total_precip >= criteria[1]:
                     WYT = 2
                 else:
                     WYT = 1
-        
+
             # July-Aug:
             else:
                 runoff = self.model.parameters["TUO_13 Headflow/Runoff"].dataframe
-                oct_1 = '{:04}-10-01'.format(date.year - 1)
                 cumulative_runoff = runoff[oct_1:date].sum()
                 cumulative_runoff *= 810.7 / 1000  # convert mcm to taf
                 if cumulative_runoff >= criteria[0]:
@@ -50,11 +51,11 @@ class IFR_bl_Hetch_Hetchy_Reservoir_Water_Year_Type(WaterLPParameter):
                     WYT = 2
                 else:
                     WYT = 1
-        
+
         self.WYT = WYT
-        
+
         return WYT
-        
+
     def value(self, timestep, scenario_index):
         try:
             return self._value(timestep, scenario_index)
@@ -72,6 +73,7 @@ class IFR_bl_Hetch_Hetchy_Reservoir_Water_Year_Type(WaterLPParameter):
             print('File where error occurred: {}'.format(__file__))
             print(err)
             raise
-        
+
+
 IFR_bl_Hetch_Hetchy_Reservoir_Water_Year_Type.register()
 print(" [*] IFR_bl_Hetch_Hetchy_Reservoir_Water_Year_Type successfully registered")
---
+++ upper_san_joaquin/_parameters/CVP_Friant_Kern_Canal_Demand.py
@@ -0,0 +1,46 @@
+from parameters import WaterLPParameter
+
+from utilities.converter import convert
+
+class CVP_Friant_Kern_Canal_Demand(WaterLPParameter):
+    """"""
+
+    def _value(self, timestep, scenario_index):
+        
+        WYT = self.get('San Joaquin Valley WYT' + self.month_suffix)
+        df = self.model.tables["CVP Friant-Kern Canal Demand"]
+        col = df.columns[WYT - 1]
+        demand_cfs = df[col]  # data is currently in cfs
+        
+        today = (self.datetime.month, self.datetime.day)
+
+        if self.model.mode == 'scheduling':
+            demand_cfs = demand_cfs[today]
+        else:
+            end = (self.datetime.month, self.days_in_month())
+            demand_cfs = demand_cfs[today:end].sum()
+        
+        demand_mcm = demand_cfs / 35.315 * 0.0864
+        
+        return demand_mcm
+        
+    def value(self, timestep, scenario_index):
+        try:
+            return convert(self._value(timestep, scenario_index), "m^3 s^-1", "m^3 day^-1", scale_in=1, scale_out=1000000.0)
+        except Exception as err:
+            print('ERROR for parameter {}'.format(self.name))
+            print('File where error occurred: {}'.format(__file__))
+            print(err)
+            raise
+
+    @classmethod
+    def load(cls, model, data):
+        try:
+            return cls(model, **data)
+        except Exception as err:
+            print('File where error occurred: {}'.format(__file__))
+            print(err)
+            raise
+        
+CVP_Friant_Kern_Canal_Demand.register()
+print(" [*] CVP_Friant_Kern_Canal_Demand successfully registered")
