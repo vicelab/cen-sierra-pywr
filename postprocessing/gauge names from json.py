1b5a6a1c647c7537fb7b852f057472a7bf32dde9
David Rheinheimer <herr.rhein@gmail.com>
2020-01-21 20:35:26 -0800

    update gauge names from json

--- postprocessing/gauge names from json.py
+++ postprocessing/gauge names from json.py
@@ -8,21 +8,32 @@ for basin in basins:
     m = json.load(open('../{}/pywr_model.json'.format(basin)))
     up_nodes = {}
     down_nodes = {}
+    nodes = {n['name']: n for n in m['nodes']}
     for n1, n2 in m['edges']:
         up_nodes[n2] = up_nodes.get(n2, []) + [n1]
         down_nodes[n1] = down_nodes.get(n1, []) + [n2]
+
+    included = []
     for node in m['nodes']:
         node_gauge_name = node.get('gauge')
         if node_gauge_name:
             gauges_list.append([node['name'], node_gauge_name])
+            included.append(node['name'])
+
+    for node in m['nodes']:
+
+        node_gauge_name = node.get('gauge')
+        if node['type'] != 'RiverGauge':
             continue
-        else:
-            if node['type'] != 'RiverGauge':
+        gauge_name = node['name']
+        gauged_node = up_nodes[up_nodes[gauge_name][0]][0]
+        if gauged_node in included:
+            # maybe it's a reservoir, so we should look down instead
+            gauged_node = down_nodes[down_nodes[gauge_name][0]][0]
+            if gauged_node in included:
                 continue
-            gauge_name = node['name']
-            up_reach = up_nodes[gauge_name][0]
-            gauged_node = up_nodes[up_reach][0]
-            gauges_list.append([gauged_node, gauge_name])
+        included.append(gauged_node)
+        gauges_list.append([gauged_node, gauge_name])
 
 with open('../dashapp/gauges.csv', 'w', newline='') as f:
     writer = csv.writer(f)
