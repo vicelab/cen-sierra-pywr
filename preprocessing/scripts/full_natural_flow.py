319c8d622d5947ffd2b00da129f3e7b7f9b2f095
David Rheinheimer <herr.rhein@gmail.com>
2020-02-05 12:28:22 -0800

    update preprocessing scripts

--- preprocessing/scripts/full_natural_flow.py
+++ preprocessing/scripts/full_natural_flow.py
@@ -5,9 +5,8 @@ import os
 def create_full_natural_flow(root_dir, basins=None):
     basins = basins or ['merced', 'stanislaus', 'upper san joaquin', 'tuolumne']
     for basin in basins:
-        if 'River' not in basin:
-            continue
-        basin_dir = os.path.join(root_dir, basin)
+        basin_full = basin.title() + " River"
+        basin_dir = os.path.join(root_dir, basin_full)
         basin_runoff_dir = os.path.join(basin_dir, 'Scenarios', 'runoff')
         if not os.path.exists(basin_runoff_dir):
             continue
@@ -26,5 +25,16 @@ def create_full_natural_flow(root_dir, basins=None):
             df.index.name = 'date'
             df.columns = ['flow']
             outdir = os.path.join(basin_dir, 'Scenarios', 'preprocessed', scenario)
+
+            # daily
             df.to_csv(os.path.join(outdir, 'full_natural_flow_daily_mcm.csv'))
-            df.resample('MS').sum().to_csv(os.path.join(outdir, 'full_natural_flow_monthly_mcm.csv'))
+
+            # monthly
+            df2 = df.resample('MS').sum()
+            df2.to_csv(os.path.join(outdir, 'full_natural_flow_monthly_mcm.csv'))
+
+            # annual
+            df3 = df2.copy()
+            df3['WY'] = [d.year if d.month <= 9 else d.year + 1 for d in df2.index]
+            df3 = df3.reset_index().set_index('WY').drop('date', axis=1).groupby('WY').sum()
+            df3.to_csv(os.path.join(outdir, 'full_natural_flow_annual_mcm.csv'))
