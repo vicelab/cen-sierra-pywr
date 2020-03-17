bf252683714547b123ac5b4c4d000a5866689da2
David Rheinheimer <herr.rhein@gmail.com>
2020-03-17 11:33:57 -0700

    move PH_Cost and PH_Water_Demand to common parameters folder

--- parameters/PH_Water_Demand.py
+++ parameters/PH_Water_Demand.py
@@ -1,15 +1,15 @@
 from parameters import WaterLPParameter
-
 from utilities.converter import convert
 from dateutil.relativedelta import relativedelta
 from calendar import isleap
 import random
+import numpy as np
 
 
 class PH_Water_Demand(WaterLPParameter):
     """"""
 
-    price_threshold = 20.0  # an arbitrary first value
+    price_threshold = None
     cms_to_mcm = 0.0864
 
     def __init__(self, model, node, block, **kwargs):
@@ -17,10 +17,16 @@ class PH_Water_Demand(WaterLPParameter):
         self.node = node
         self.block = block
 
-    def _value(self, timestep, scenario_index, mode='scheduling'):
-        kwargs = dict(timestep=timestep, scenario_index=scenario_index)
+    def setup(self):
+        super().setup()
+        num_scenarios = len(self.model.scenarios.combinations)
+        self.price_threshold = np.zeros([num_scenarios], np.float)
+
+    def _value(self, timestep, scenario_index):
+
+        sid = scenario_index.global_id
+
         all_energy_prices = self.model.tables['All Energy Price Values']
-        param = self.res_name + '/Turbine Capacity' + self.month_suffix
         powerhouse = self.model.nodes[self.res_name + self.month_suffix]  # powerhouse
         turbine_capacity_mcm = powerhouse.turbine_capacity
 
@@ -52,7 +58,7 @@ class PH_Water_Demand(WaterLPParameter):
                     price_end = end.strftime('{}-%m-%d'.format(price_year))
                 energy_prices = all_energy_prices[price_date:price_end].values.flatten()
                 energy_prices[::-1].sort()  # sort in descending order
-                planning_release = self.model.planning.nodes[self.res_name + '/1'].flow[-1]
+                planning_release = self.model.planning.nodes[self.res_name + '/1'].flow[scenario_index.global_id]
 
                 # for planning turbine capacity, note that the turbine capacities are the same
                 # in both models (i.e., cms)
@@ -60,16 +66,16 @@ class PH_Water_Demand(WaterLPParameter):
                 planning_release_fraction = min(planning_release / planning_turbine_capacity, 1.0)
                 price_index = int(len(energy_prices) * planning_release_fraction) - 1
                 if price_index < 0:
-                    self.price_threshold = 1e6  # no production this month (unlikely)
+                    self.price_threshold[sid] = 1e6  # no production this month (unlikely)
                 else:
-                    self.price_threshold = energy_prices[price_index]
+                    self.price_threshold[sid] = energy_prices[price_index]
 
             # calculate today's total release
             energy_prices_today = all_energy_prices.loc[price_date].values
             if self.block == 1:
-                production_hours = len([1 for p in energy_prices_today if p >= self.price_threshold])
+                production_hours = len([1 for p in energy_prices_today if p >= self.price_threshold[sid]])
             else:
-                production_hours = len([1 for p in energy_prices_today if 0.0 < p < self.price_threshold])
+                production_hours = len([1 for p in energy_prices_today if 0.0 < p < self.price_threshold[sid]])
 
             max_flow_fraction = production_hours / 24
             # blocks = self.model.tables["Energy Price Blocks"].loc[timestep.datetime]
@@ -98,9 +104,9 @@ class PH_Water_Demand(WaterLPParameter):
 
         return demand_mcm
 
-    def value(self, timestep, scenario_index):
+    def value(self, *args, **kwargs):
         try:
-            return self._value(timestep, scenario_index, mode=self.mode)
+            return self._value(*args, **kwargs)
         except Exception as err:
             print('\nERROR for parameter {}'.format(self.name))
             print('File where error occurred: {}'.format(__file__))
