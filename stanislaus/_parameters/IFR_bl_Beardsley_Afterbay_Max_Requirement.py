6cc7763a5cb03047faa10ffb652354029a817a25
David Rheinheimer <herr.rhein@gmail.com>
2020-05-14 17:00:52 -0700

    fix stray dedent

--- stanislaus/_parameters/IFR_bl_Beardsley_Afterbay_Max_Requirement.py
+++ stanislaus/_parameters/IFR_bl_Beardsley_Afterbay_Max_Requirement.py
@@ -16,7 +16,8 @@ class IFR_bl_Beardsley_Afterbay_Max_Requirement(FlowRangeParameter):
     def value(self, timestep, scenario_index):
         val = self.requirement(timestep, scenario_index, default=self._value)
         return convert(val, "m^3 s^-1", "m^3 day^-1", scale_in=1, scale_out=1000000.0)
-@classmethod
+
+    @classmethod
     def load(cls, model, data):
         return cls(model, **data)
 
