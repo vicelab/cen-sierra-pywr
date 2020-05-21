42e105a7a87b8b011d39e6b76e8abc6bbfd92048
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2020-05-21 14:58:29 -0700

    made specifications to client

--- scripts/interactive_runs.py
+++ scripts/interactive_runs.py
@@ -3,18 +3,17 @@ import time
 import socket
 import dask.distributed
 
-# from dask_jobqueue import SLURMCluster
-# cluster = SLURMCluster(
-# queue='fast.q', # or std.q, preempt.q, gpu.q...
-# project="",
-# cores=1, #  up to 24
-# memory="10 GB" # up to 240 on merced.
-# )
+
 
 hostname = socket.gethostname()
 if hostname.startswith('merced') or hostname.startswith('mrcd'):
     from dask_jobqueue import SLURMCluster
-    cluster = SLURMCluster(...)
+    cluster = SLURMCluster(
+    queue='fast.q', # or std.q, preempt.q, gpu.q...
+    project="",
+    cores=1, #  up to 24
+    memory="10 GB" # up to 240 on merced.
+)
 else:
     cluster = None
 
