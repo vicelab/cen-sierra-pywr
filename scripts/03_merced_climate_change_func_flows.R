6ad2162466f196ea54d00dcd1452744d9369463b
Ann Willis <awillis@ucdavis.edu>
2020-11-19 13:24:48 -0800

    Completed and saved the ff analysis for CNRM-CM5 hydrology

--- scripts/03_merced_climate_change_func_flows.R
+++ scripts/03_merced_climate_change_func_flows.R
@@ -29,6 +29,8 @@ mer_cesm1_bgc_mcm <- read_csv("functional_flows/climate_change/Merced/CESM1-BGC_
 
 mer_cmcc_cms_mcm <- read_csv("functional_flows/climate_change/Merced/CMCC-CMS_rcp85/preprocessed/full_natural_flow_daily_mcm.csv")
 
+mer_cnrm_cm5_mcm <- read_csv("functional_flows/climate_change/Merced/CNRM-CM5_rcp85/preprocessed/full_natural_flow_daily_mcm.csv")
+
 # Unit conversion of gmc hydrology ----------------------------------------
 
 # Climate change flow files are currently in mcm (million cubic meters per day) - need to convert to cfs to match the functional flow recommendations.
@@ -108,6 +110,21 @@ mer_cmcc_cms_cfs <- mer_cmcc_cms_mcm %>%
 
 mer_cmcc_cms_cfs$date <- as.character(mer_cmcc_cms_cfs$date)
 
+#CNRM-CM5 gcm
+mer_cnrm_cm5_mcm <- mer_cnrm_cm5_mcm %>% 
+  rename("flow_mcm" = "flow")
+
+mer_cnrm_cm5_mcm$flow_cfs <- mer_cnrm_cm5_mcm$flow_mcm/.0864*35.315
+
+# make new df of cfs flows, check columns for NAs
+
+mer_cnrm_cm5_cfs <- mer_cnrm_cm5_mcm %>% 
+  select(date, flow_cfs) %>% 
+  filter(!is.na(flow_cfs)) %>% 
+  filter(!is.na(date))
+
+mer_cnrm_cm5_cfs$date <- as.character(mer_cnrm_cm5_cfs$date)
+
 # Observed functional flows ------------------------------------------------
 
 ffc <- FFCProcessor$new()  # make a new object we can use to run the commands
@@ -180,6 +197,17 @@ ffc_cc$step_one_functional_flow_results(timeseries = mer_cmcc_cms_cfs,
                                         token=Ann_token, 
                                         comid=mer_comid,
                                         output_folder = "functional_flows/climate_change/Merced/CMCC-CMS_rcp85/functional_flow_analysis/")
+
+ffc_cc$step_two_explore_ecological_flow_criteria()
+
+ffc_cc$step_three_assess_alteration()
+
+# CNRM-CM5 functional flows ------------------------------------------------
+
+ffc_cc$step_one_functional_flow_results(timeseries = mer_cnrm_cm5_cfs, 
+                                        token=Ann_token, 
+                                        comid=mer_comid,
+                                        output_folder = "functional_flows/climate_change/Merced/CNRM-CM5_rcp85/functional_flow_analysis/")
 
 ffc_cc$step_two_explore_ecological_flow_criteria()
 
