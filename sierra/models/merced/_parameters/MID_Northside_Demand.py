3bf2c30b31cf7271491ed46a14da6974b421dddc
Gustavo Facincani Dourado <57502337+GustavoFacincani@users.noreply.github.com>
2021-04-11 22:07:05 -0700

    Update MID_Northside_Demand.py
    Increasing the deliveries a little bit, to reduce the bias in the results.

--- sierra/models/merced/_parameters/MID_Northside_Demand.py
+++ sierra/models/merced/_parameters/MID_Northside_Demand.py
@@ -10,9 +10,11 @@ class MID_Northside_Demand(WaterLPParameter):
 
     def _value(self, timestep, scenario_index):
 
+        MID_North_bias_factor = 1.06
+
         WYT = self.model.tables['WYT for IFR Below Exchequer'][self.operational_water_year]
         ts = "{}/{}/1900".format(timestep.month, timestep.day)
-        demand_cms = self.model.tables["MID Northside Diversions"].at[ts, WYT] / 35.31
+        demand_cms = self.model.tables["MID Northside Diversions"].at[ts, WYT]*self.MID_North_bias_factor / 35.31
 
         return demand_cms
 
