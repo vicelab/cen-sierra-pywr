cdbe9c18b72f9f66cb9d0f3d83979cf878dd380e
Gustavo Facincani Dourado <57502337+GustavoFacincani@users.noreply.github.com>
2021-04-11 22:30:29 -0700

    Update Oakdale_Irrigation_District_Demand.py

--- sierra/models/stanislaus/_parameters/Oakdale_Irrigation_District_Demand.py
+++ sierra/models/stanislaus/_parameters/Oakdale_Irrigation_District_Demand.py
@@ -4,13 +4,13 @@ from dateutil.relativedelta import relativedelta
 
 class Oakdale_Irrigation_District_Demand(WaterLPParameter):
 
+    OID_bias_factor = 1.02
+    
     def _value(self, timestep, scenario_index):
 
         if not 3 <= self.datetime.month <= 10:
             return 0  # Only deliver Mar-Oct based on observed data
         
-        OID_bias_factor = 1.02
-
         WYT = self.get('San Joaquin Valley WYT' + self.month_suffix, timestep, scenario_index)
         demand_mcm_df = self.model.tables["Oakdale Irrigation District Demand"][WYT]*self.OID_bias_factor
         start = (self.datetime.month, self.datetime.day)
