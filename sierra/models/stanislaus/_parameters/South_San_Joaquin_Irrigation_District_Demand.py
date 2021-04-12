60e42647aae37fd7e2aabe0c49a5174491b2d23d
Gustavo Facincani Dourado <57502337+GustavoFacincani@users.noreply.github.com>
2021-04-11 22:14:04 -0700

    Update South_San_Joaquin_Irrigation_District_Demand.py
    Increasing the deliveries a little bit, to reduce the bias in the results.

--- sierra/models/stanislaus/_parameters/South_San_Joaquin_Irrigation_District_Demand.py
+++ sierra/models/stanislaus/_parameters/South_San_Joaquin_Irrigation_District_Demand.py
@@ -8,8 +8,10 @@ class South_San_Joaquin_Irrigation_District_Demand(WaterLPParameter):
         if not 3 <= self.datetime.month <= 10:
             return 0  # Only deliver Mar-Oct based on observed data
 
+        SSJID_bias_factor = 1.07
+
         WYT = self.get('San Joaquin Valley WYT' + self.month_suffix, timestep, scenario_index)
-        demand_mcm_df = self.model.tables["South San Joaquin Irrigation District Demand"][WYT]
+        demand_mcm_df = self.model.tables["South San Joaquin Irrigation District Demand"][WYT]*self.SSJID_bias_factor
         start = (self.datetime.month, self.datetime.day)
         if self.model.mode == 'scheduling':
             demand_mcm = demand_mcm_df[start]
