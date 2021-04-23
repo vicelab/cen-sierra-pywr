58aa2f7881728239eb97d8c9e26adbac599697e9
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-04-23 18:44:31 -0500

    modified setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
+
+# extensions = [Extension("*", ["*.pyx"])]
 
 setup(
     name='sierra-pywr',
-    # cmdclass = {'build_ext': build_ext},
-    # ext_modules = cythonize("/sierra/base_parameters/*.pyx", compiler_dirctives={'language_level': 3}),
-    ext_modules = cythonize(["*.pyx"])
-    ext_modules = cythonize("/sierra-cython/*")
-    # zip_safe = False,
+    ext_modules = cythonize("sierra_cython/models/{}", ["*.pyx"]),
+    zip_safe = False,
 )
