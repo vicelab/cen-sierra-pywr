bbb561655e0b4d2e126d40d400775c5ce44c5ec6
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-04-29 01:02:12 -0500

    fixed setup

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-from distutils.extension import Extension
-
-# extensions = [Extension("*", ["*.pyx"])]
 
 setup(
-    name='sierra-pywr',
-    ext_modules = cythonize("sierra_cython/models/{}", ["*.pyx"]),
-    zip_safe = False,
+  name = 'MyProject',
+  ext_modules = cythonize(["*.pyx"]),
 )
