55f4327d67cf3837d4d17d8458cd3bac032a44ab
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2021-05-30 12:53:48 -0700

    Imported scripts specified to user: Lorezno

---
+++ scripts/batch files/hpc.sierra-pywr_stanislaus.bat
@@ -0,0 +1,31 @@
+#! /bin/bash -l
+
+#SBATCH -J sierra-pywr
+#SBATCH -o sierra-pywr.out
+#SBATCH -e sierra-pywr.error
+#SBATCH -n 1
+#SBATCH -p fast.q
+#SBATCH -t 00:15:00
+#SBATCH --mail-user=lscaturchio@ucmerced.edu
+#SBATCH --mail-type=begin,end,fail
+
+module load anaconda3
+echo 'Loaded anaconda module'
+module load gcc-10.2.0
+echo 'Loaded gcc module'
+module load glpk/glpk-5.0 
+echo 'Loaded glpk-5.0 module'
+
+conda activate sierra-pywr
+echo 'sierra-pywr environment initialized'
+
+export SIERRA_DATA_PATH="/home/lscaturchio/data"
+echo 'Exported SIERRA_DATA_PATH'
+export SIERRA_RESULTS_PATH="@169.236.225.129:/VICElab/RESEARCH/PROJECTS/CERC-WET/Task7_San_Joaquin_Model/pywr_models/results"
+echo 'Exported SIERRA_RESULTS_PATH'
+
+cd sierra-pywr
+
+python3 main.py -b stanislaus -mp joblib
+
+rsync ~/hi.txt -zaP lscaturchio@169.236.225.129:/VICElab/RESEARCH/PROJECTS/CERC-WET/Task7_San_Joaquin_Model/pywr_models/data
